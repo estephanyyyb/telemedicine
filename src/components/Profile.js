dfbc1547ed92381337369ada6a0109580feee7fe
MikeLarsenW <mike.wessels8@gmail.com>
2021-10-28 11:03:05 -0400

    Presentation 2 final draft

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -72,7 +72,6 @@ class Profile extends React.Component {
                 <div>
                     <PageHeader currentUser={this.props.currentUser}></PageHeader>
                     <br/>
-
                     <h1 className={profileStyle.h1}>Profile</h1>
                     <div className={ reveal ? `${profileStyle['profile-box']} ${profileStyle['primary-color']}` : `${profileStyle['profile-box2']} ${profileStyle['primary-color']}`}>
                         <div className={ reveal ? `${profileStyle['profile-textbox']}` : `${profileStyle['profile-textbox2']}`}>
