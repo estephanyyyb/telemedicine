d74acb77183a1c24efc83ff45338776b40f20d54
Cade Lanktree <cadelanktree@Cades-Air.attlocal.net>
2021-11-27 12:16:02 -0500

    first commit on this branch

--- src/components/recordings/DoctorRecordings.js
+++ src/components/recordings/DoctorRecordings.js
@@ -1,218 +1,266 @@
-import React , {useState, useEffect} from 'react';
+import React , {useState, useEffect, Component} from 'react';
 import telemedicineLogo from '../../images/telemedicineLogo2.png';
 import userIcon from '../../images/userIcon1.png';
 import { AmplifySignOut } from '@aws-amplify/ui-react';
 import PropTypes from 'prop-types';
 import styles from './DoctorRecordings.css'
 // import AWS from 'aws-sdk';
-
-const AWS = require('aws-sdk');
-const S3 = new AWS.S3(); // YOU NEED THIS LINE
-
-
-// const AWS_ACCESS_KEY_ID = 'AKIAZ7RYNOYKJCH42GGR';
-// const AWS_SECRET_ACCESS_KEY = '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';
-const REGION ='us-east-1';
-const S3_BUCKET ='recordings-staging-telemedicine5a';
+import firebase from "firebase/app";
+import 'firebase/storage';
 
 
-// Set up array for HTML table using heading and body vars
-var heading = ['File', 'Date', 'Length'];
-var body = ['testvideo1.mov','testvideo1.mov','testvideo1.mov','testvideo1.mov','testvideo1.mov'];
-
-var objectUrls = [];
-var objectName = [];
-var objectLength =[]; //or date?
+const BUCKET = '';
 
 
-// require('dotenv').config();
-// AWS.config.update({
-//   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-//   secretAccessKey: process.env.SECRET_ACCESS_KEY,
-//   region: REGION
-// })
+const firebaseConfig = {
+  apiKey: "AIzaSyAwxvSMHLyXiEKTBn4D-L8llyoYu-K8Yqw",
+  authDomain: "telemedicine-c0afa.firebaseapp.com",
+  projectId: "telemedicine-c0afa",
+  storageBucket: "telemedicine-c0afa.appspot.com",
+  messagingSenderId: "404118376728",
+  appId: "1:404118376728:web:500e4dca21d7d18f62ce6d",
+  measurementId: "G-4WQP69JN8X"
+}
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}else {
+  firebase.app(); // if already initialized, use that one
+}
+const storage = firebase.storage();
+
+const DoctorRecordings = (props) => { 
+  const heading = ['File', 'Time Created', 'Size', 'File URL'];
+  const [body, setBodyArray] = useState([]); //declare body array
+  const [name, setNameArray] = useState([]); //declare name array
+  const [time, setTimeArray] = useState([]); //declare time array
+  const [size, setSizeArray] = useState([]); //declare size array
+  const [URL, setURLArray] = useState([]); //declare URL array
+  const [user, setUser] = React.useState();
+  const [show, setShow] = useState(false);
+  const [value, setValue] = useState('');
+  const [url, setUrl] = useState('');
+  const [data, setData] = useState([]);
+  const [image , setImage] = useState('');
+  const upload = ()=>{
+    if(image == null)
+      return;
+    // setUrl("Getting download link...");
+    storage.ref(`/images/${image.name}`).put(image)
+    .on("state_changed" , alert("success") , alert);
+
+    //Get Link
+    // storage.ref("images").child(image.name).getDownloadURL()
+    // .then((url) => {
+    //   setUrl(url);
+    // })
+    
+  }
+  
+ //List Items in Storage
+  useEffect(() => {
+    storage.ref().child('images/').listAll()
+    .then(res => {
+      res.items.forEach((item) => {
+       setNameArray(name => [...name, item.name])
+      })
+    })
+    .catch(err=> {
+      alert(err.message);
+    })
+
+  }, [])
 
 
-// // Initialize the Amazon Cognito credentials provider
-// AWS.config.region = 'us-east-1'; // Region
-// AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-//     IdentityPoolId: 'us-east-1:53b3fb4a-cdc7-4113-bb71-9023ba101108',
-// });
+  useEffect(()  => {
+    storage.ref().child('images/').listAll()
+    .then(res => {
+      res.items.forEach((item) => {
+        item.getMetadata()
+        .then(res3 => {
+          setSizeArray(time => [...time, res3.timeCreated]);
+        })
+      })
+    })
+  }, [])
+
+//SIZE -------------------- SIZE
+useEffect(()  => {
+  storage.ref().child('images/').listAll()
+  .then(res => {
+    res.items.forEach((item) => {
+      item.getMetadata()
+      .then(res3 => {
+        setSizeArray(size => [...size, res3.size]);
+      })
+    })
+  })
+}, [])
 
 
-// AWS.config.update({
-//   region: 'us-east-1',
-//   credentials: new AWS.CognitoIdentityCredentials({
-//     IdentityPoolId: 'us-east-1:53b3fb4a-cdc7-4113-bb71-9023ba101108'
-//   })
-// });
 
-
-// // Ideal way to load keys; keep here ...
-// AWS.config.getCredentials(function(err) {
-//   if (err) console.log(err.stack);
-//   // credentials not loaded
-//   else {
-//     console.log("Access key:", AWS.config.credentials.accessKeyId);
-//   }
-// });
-
-
-
-
-
-const myBucket = new AWS.S3({
-  params: { Bucket: S3_BUCKET },
-  region: REGION,
-})
-
-
-const DoctorRecordings = (props) => {
-
-    const [progress , setProgress] = useState(0);
-    const [selectedFile, setSelectedFile] = useState(null);
-
-    const handleFileInput = (e) => {
-        setSelectedFile(e.target.files[0]);
-    }
-
-    const uploadFile = (file) => {
-
-        const params = {
-            ACL: 'public-read',
-            Body: file,
-            Bucket: S3_BUCKET,
-            Key: file.name
-        };
-
-        myBucket.putObject(params)
-            .on('httpUploadProgress', (evt) => {
-                setProgress(Math.round((evt.loaded / evt.total) * 100))
+  //DOWNLOAD URLS
+  useEffect(() => {
+    storage.ref().child('images/').listAll()
+    .then(res => {
+        res.items.forEach(item => {
+            item.getDownloadURL()
+            .then(url => {
+              setURLArray(URL => [...URL, url]);
             })
-            .send((err) => {
-                if (err) console.log(err)
-                else console.log('Successful upload!')
-            })
-    }
-
-    (async function() { //FUNCTION IS GOOD CAUSE IT WORKED WITH .listObjectsV2 multiple refreshes
-      
-      try {
-
-        AWS.config.setPromisesDependency();
-
-        const response = await S3.listObjectsV2({
-          Bucket: S3_BUCKET
-        }).promise();
-        
-
-        // var key = response.Contents[0].Key;
-        // var paramsTwo = {Bucket: 'bucket', Key: key};
-        // var url = S3.getSignedUrl('getObject', paramsTwo); //ERROR ISN'T getSignedURL
-        // console.log('The URL is: ', url);
-        // body.push(response.Contents[0].Key);
-        // console.log(response.Contents[0]);
-
-        
-      } catch(e) {
-        console.log('our error', e);
-      }
-
-    })();
-
-    for (var i = 0; i < body.length; i++) {
-      console.log(body[i]);
-    }
+        })
+    })
+  }, [])
 
 
+  function deleteObjectandRow() {
+    // delete object and html row when button clicked
+  }
 
 
 
 
     return (
-      <div>
+      <div className="App">
       {/* <PageHeader currentUser={props.currentUser}></PageHeader> */}
-      <div>
-    <nav className="navbar navbar-light bg-light">
+       <div>
+      <nav className="navbar navbar-light bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand brand-text" href="/">
-          <img src={telemedicineLogo} alt="" width="25" height="25" className="d-inline-block align-text-top" />
-          Telemedicine
-        </a>
-        <div className="btn-group">
-          <button type="button" className="btn btn-light"><img className="d-inline-block align-text-top" src={userIcon} alt="" width="20" height="20" />
-            {" Dr. " + props.currentUser}
-          </button>
+         <a className="navbar-brand brand-text" href="/">
+           <img src={telemedicineLogo} alt="" width="25" height="25" className="d-inline-block align-text-top" />
+           Telemedicine
+         </a>
+         <div className="btn-group">
+           <button type="button" className="btn btn-light"><img className="d-inline-block align-text-top" src={userIcon} alt="" width="20" height="20" />
+             {" Dr. " + props.currentUser}
+           </button>
           <button type="button" className="btn btn-light dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
-            <span className="visually-hidden">Toggle Dropdown</span>
-          </button>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Profile</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
-            <li><hr className="dropdown-divider"></hr></li>
-            <li><a className="dropdown-item" href="#"><AmplifySignOut /></a></li>
-          </ul>
-        </div>
+             <span className="visually-hidden">Toggle Dropdown</span>
+           </button>
+           <ul className="dropdown-menu">
+             <li><a className="dropdown-item" href="#">Profile</a></li>
+             <li><a className="dropdown-item" href="#">Another action</a></li>
+             <li><a className="dropdown-item" href="#">Something else here</a></li>
+             <li><hr className="dropdown-divider"></hr></li>
+             <li><a className="dropdown-item" href="#"><AmplifySignOut /></a></li>
+           </ul>
+         </div>
+       </div>
+     </nav>
+     <div className="d-flex justify-content-evenly navbar primary-color">
+       <button type="button" className="btn btn-secondary btn-sm" href="/reports">Reports</button>
+       <button type="button" className="btn btn-secondary btn-sm">Messages</button>
+       <button type="button" className="btn btn-secondary btn-sm">Appointments</button>
+       <button type="button" className="btn btn-secondary btn-sm">Recordings</button>
+       <span className="navbar-brand mb-0 h1"></span>
+     </div>
+   </div>
+   <br/><br/>
+       <h1><strong>Upload a new Recording</strong></h1>
+        <center>
+        <input type="file" onChange={(e)=>{setImage(e.target.files[0])}}/>
+        <button onClick={upload}>Upload</button>
+        <br/> <br/>
+        <br/> <br/>
+        <br/> <br/>
+
+        {/* <div >
+          <Table heading={heading} body={body} />,
+        </div> */}
+
+          <table>
+                  <thead>
+                      <tr>
+                          {heading.map(head => <th>{head}</th>)}
+                      </tr>
+                  </thead>
+                    <tr> 
+                      {name.map(name => <td> {name} </td>)}
+                      {/* {time.map(time => <td> {time} </td>)} */}
+                      {size.map(size => <td> {size} </td>)}
+                      {URL.map(URL => <td> {URL} </td>)}
+                    </tr>
+          </table>
+        </center>
       </div>
-    </nav>
-    <div className="d-flex justify-content-evenly navbar primary-color">
-      <button type="button" className="btn btn-secondary btn-sm" href="/reports">Reports</button>
-      <button type="button" className="btn btn-secondary btn-sm">Messages</button>
-      <button type="button" className="btn btn-secondary btn-sm">Appointments</button>
-      <button type="button" className="btn btn-secondary btn-sm">Recordings</button>
-      <span className="navbar-brand mb-0 h1"></span>
-    </div>
-  </div>
-  <br/><br/>
-      <h1><strong>Upload a new Recording</strong></h1>
-      <div id="upload-card">
-      <div>Native SDK File Upload Progress is {progress}%</div>
-      <input type="file" onChange={handleFileInput}/><br/>
-      <button onClick={() => uploadFile(selectedFile)}> Upload to S3</button>
-      </div>
-      <br/><br/><br/><br/>
-      <h1><strong>Video Recordings: Bucket Objects</strong></h1>
-      <table id="recordings" style={{width: 1000}}>
-        <thead>
-          <tr> 
-            <th>File</th><th>Last Modified</th><th>Size</th>
-          </tr>
-        </thead>
-          <tr>
-            <th><a href="https://recordings-staging-telemedicine5a.s3.amazonaws.com/testvideo1.mov">testvideo1.mov</a></th><th>October 28, 2021, 04:07:46 (UTC-04:00)</th><th>822.3 KB</th>
-          </tr>
-          <tr>
-          <th><a href="https://recordings-staging-telemedicine5a.s3.amazonaws.com/testvideo2.mov">testvideo2.mov</a></th><th>October 28, 2021, 04:07:45 (UTC-04:00)</th><th>1.4 MB</th>
-          </tr>
-          <tr>
-          <th><a href="https://recordings-staging-telemedicine5a.s3.amazonaws.com/testvideo3.mov">testvideo3.mov</a></th><th>October 28, 2021, 04:07:45 (UTC-04:00)</th><th>627.4 KB</th>
-          </tr>
-          <tr>
-          <th><a href="https://recordings-staging-telemedicine5a.s3.amazonaws.com/testvideo4.mov">testvideo4.mov</a></th><th>October 28, 2021, 04:07:44 (UTC-04:00)</th><th>1.5 MB</th>
-          </tr>
-          <tr>
-          <th><a href="https://recordings-staging-telemedicine5a.s3.amazonaws.com/testvideo5.mov">testvideo5.mov</a></th><th>October 28, 2021, 04:07:44 (UTC-04:00)</th><th>2.1 MB</th>
-          </tr>
-        
-      </table>
-      {/* <table style={{width: 500}}>
-        <thead>
-          <tr>
-            {heading.map(head => <th>{head}</th>)}
-          </tr>
-        </thead>
-        <tbody>
-          {body.map(row => <td> {row} </td>)}
-        </tbody>
-      </table> */}
-      </div>
-        
     );
 
+
+
+
+
+
+//     return (
+//       <div>
+//       {/* <PageHeader currentUser={props.currentUser}></PageHeader> */}
+//       <div>
+//     <nav className="navbar navbar-light bg-light">
+//       <div className="container-fluid">
+//         <a className="navbar-brand brand-text" href="/">
+//           <img src={telemedicineLogo} alt="" width="25" height="25" className="d-inline-block align-text-top" />
+//           Telemedicine
+//         </a>
+//         <div className="btn-group">
+//           <button type="button" className="btn btn-light"><img className="d-inline-block align-text-top" src={userIcon} alt="" width="20" height="20" />
+//             {" Dr. " + props.currentUser}
+//           </button>
+//           <button type="button" className="btn btn-light dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+//             <span className="visually-hidden">Toggle Dropdown</span>
+//           </button>
+//           <ul className="dropdown-menu">
+//             <li><a className="dropdown-item" href="#">Profile</a></li>
+//             <li><a className="dropdown-item" href="#">Another action</a></li>
+//             <li><a className="dropdown-item" href="#">Something else here</a></li>
+//             <li><hr className="dropdown-divider"></hr></li>
+//             <li><a className="dropdown-item" href="#"><AmplifySignOut /></a></li>
+//           </ul>
+//         </div>
+//       </div>
+//     </nav>
+//     <div className="d-flex justify-content-evenly navbar primary-color">
+//       <button type="button" className="btn btn-secondary btn-sm" href="/reports">Reports</button>
+//       <button type="button" className="btn btn-secondary btn-sm">Messages</button>
+//       <button type="button" className="btn btn-secondary btn-sm">Appointments</button>
+//       <button type="button" className="btn btn-secondary btn-sm">Recordings</button>
+//       <span className="navbar-brand mb-0 h1"></span>
+//     </div>
+//   </div>
+//   <br/><br/>
+
+
+
+}
+  export {
+    storage, firebase, DoctorRecordings as default
   }
 
 
-  export default DoctorRecordings;
-
-
+//   class Table extends Component {
+//     render() {
+//         var heading = this.props.heading;
+//         var body = this.props.body;
+//         return (
+//             <table style={{ width: 500 }}>
+//                 <thead>
+//                     <tr>
+//                         {heading.map(head => <th>{head}</th>)}
+//                     </tr>
+//                 </thead>
+//                 <tbody>
+//                     {body.map(row => <TableRow row={row} />)}
+//                 </tbody>
+//             </table>
+//         );
+//     }
+// }
+  
+// class TableRow extends Component {
+//     render() {
+//         var row = this.props.row;
+//         return (
+//             <tr>
+//                 {row.map(val => <td>{val}</td>)}
+//             </tr>
+//         )
+//     }
+// }
